d9846527b1bc46916550508dd4089411fb5c123d
Michael Fuller <ful422.m@gmail.com>
2024-02-24 03:02:01 +0000

    refactor vmagent_api, moving it to pkg directory and out of main.go

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import "time"
-
-type VMAgentAPIResponse struct {
-	Data   Data   `json:"data"`
-	Status string `json:"status"`
-}
-type VMAgentAPITarget struct {
-	DiscoveredLabels   map[string]string `json:"discoveredLabels"`
-	Labels             map[string]string `json:"labels"`
-	ScrapePool         string            `json:"scrapePool"`
-	ScrapeURL          string            `json:"scrapeUrl"`
-	LastError          string            `json:"lastError"`
-	LastScrape         time.Time         `json:"lastScrape"`
-	LastScrapeDuration float64           `json:"lastScrapeDuration"`
-	LastSamplesScraped int               `json:"lastSamplesScraped"`
-	Health             string            `json:"health"`
-}
-type Data struct {
-	ActiveTargets  []VMAgentAPITarget `json:"activeTargets"`
-	DroppedTargets []VMAgentAPITarget `json:"droppedTargets"`
-}
-
 func main() {
 	panic("ope")
 }
