9cecfebf734929f029e3935e94f4497649de9ded
Michael Fuller <ful422.m@gmail.com>
2024-02-24 02:55:59 +0000

    pass Test_fetchVMAgentTargets by filling in some missing details

--- test_helpers.go
+++ test_helpers.go
@@ -11,6 +11,7 @@ import (
 
 const (
 	happySampleText = `{"status":"success","data":{"activeTargets":[{"discoveredLabels":{"__address__":"nodeexporter:9100","__metrics_path__":"/metrics","__scheme__":"http","__scrape_interval__":"5s","__scrape_timeout__":"5s","job":"int_node_exporter"},"labels":{"instance":"nodeexporter:9100","job":"int_node_exporter"},"scrapePool":"int_node_exporter","scrapeUrl":"http://nodeexporter:9100/metrics","lastError":"","lastScrape":"2024-02-24T02:43:09.715Z","lastScrapeDuration":0.143,"lastSamplesScraped":918,"health":"up"}],"droppedTargets":[]}}`
+	happyScrapeTime = `2024-02-24T02:43:09.715Z`
 )
 
 func genTestHappyPathServerReq(t *testing.T) (*httptest.Server, *http.Request) {
--- vmagent_api_test.go
+++ vmagent_api_test.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
 
 func Test_fetchVMAgentTargets(t *testing.T) {
+	hst, _ := time.Parse(time.RFC3339, happyScrapeTime)
 	type args struct {
 		c *http.Client
 	}
@@ -23,7 +25,32 @@ func Test_fetchVMAgentTargets(t *testing.T) {
 				c: http.DefaultClient,
 			},
 			want: VMAgentAPIResponse{
-				Data:   Data{},
+				Data: Data{
+					ActiveTargets: []VMAgentAPITarget{
+						{
+							DiscoveredLabels: map[string]string{
+								"__address__":         "nodeexporter:9100",
+								"__metrics_path__":    "/metrics",
+								"__scheme__":          "http",
+								"__scrape_interval__": "5s",
+								"__scrape_timeout__":  "5s",
+								"job":                 "int_node_exporter",
+							},
+							Labels: map[string]string{
+								"job":      "int_node_exporter",
+								"instance": "nodeexporter:9100",
+							},
+							ScrapePool:         "int_node_exporter",
+							ScrapeURL:          "http://nodeexporter:9100/metrics",
+							LastError:          "",
+							LastScrape:         hst,
+							LastScrapeDuration: 0.143,
+							LastSamplesScraped: 918,
+							Health:             "up",
+						},
+					},
+					DroppedTargets: []VMAgentAPITarget{},
+				},
 				Status: "success",
 			},
 			wantErr: nil,
