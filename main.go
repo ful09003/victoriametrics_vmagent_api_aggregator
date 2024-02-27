345ba4d756f8871ba98fc729dc762d66dd12b847
Michael Fuller <ful422.m@gmail.com>
2024-02-27 04:11:21 +0000

    main.go: try a lil fun

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ful09003/victoriametrics_vmagent_api_aggregator/pkg"
+)
+
+var (
+	flagVMAgentTargets       = flag.String("targetdiscovery", "VAA_", "location to watch for target discovery. accepts a file path or prefix for env var")
+	flagTargetsWatchInterval = flag.Duration("discoveryinterval", 10*time.Second, "duration to update discovered targets list")
+)
+
 func main() {
-	panic("ope")
+	flag.Parse()
+	disco := newDiscovery(*flagVMAgentTargets)
+	endpoints, err := disco.DiscoverEndpoints()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	collection, err := pkg.NewVMAgentCollection(endpoints)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Printf("%+v\n", collection)
+		os.Exit(1)
+	}()
+
+	t := time.NewTicker(*flagTargetsWatchInterval)
+	for range t.C {
+		if errs := collection.CollectAll(); len(errs) != 0 {
+			log.Println(errs)
+		}
+	}
+}
+
+func newDiscovery(s string) pkg.VMAgentDiscoverer {
+	if _, err := os.Stat(s); err == nil {
+		return pkg.NewFileDiscovery(s)
+	}
+	return pkg.NewEnvDiscovery(s)
 }
